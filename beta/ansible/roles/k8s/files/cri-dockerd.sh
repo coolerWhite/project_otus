02f10236605529558075e883059681c50f3eb64e
coolerWhite <ronaldinio09@yandex.ru>
2023-07-07 18:22:45 +0300

    переместил все в beta

--- beta/ansible/roles/k8s/files/cri-dockerd.sh
+++ beta/ansible/roles/k8s/files/cri-dockerd.sh
@@ -1,8 +1,6 @@
+cd ~
 git clone https://github.com/Mirantis/cri-dockerd.git
-
-# Run these commands as root
-cd cri-dockerd
-sudo mkdir -p /usr/local/bin
+sudo mv cri-dockerd /usr/local/bin
 sudo install -o root -g root -m 0755 cri-dockerd /usr/local/bin/cri-dockerd
 sudo install packaging/systemd/* /etc/systemd/system
 sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
